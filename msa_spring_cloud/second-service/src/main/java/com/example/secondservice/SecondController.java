8e17b0d93aba3cc30c6ae94a2464f2db6d832587
Yuhojae <ho_0214@naver.com>
2023-10-10 18:01:12 +0900

    api gateway 설정중

--- msa_spring_cloud/second-service/src/main/java/com/example/secondservice/SecondController.java
+++ msa_spring_cloud/second-service/src/main/java/com/example/secondservice/SecondController.java
@@ -1,14 +1,22 @@
 package com.example.secondservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class SecondController {
     @GetMapping("/welcome")
     public String welcome(){
         return "welcome second service";
+    }
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-header") String header){
+        log.info(header);
+        return "Hello World in Second Service.";
     }
 }
