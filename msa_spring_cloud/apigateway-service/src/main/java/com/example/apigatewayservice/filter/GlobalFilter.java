044d422938a8d9bd4471ec6ac35bc873d1162ea8
Yuhojae <ho_0214@naver.com>
2023-10-11 17:58:04 +0900

    global config

--- msa_spring_cloud/apigateway-service/src/main/java/com/example/apigatewayservice/filter/GlobalFilter.java
+++ msa_spring_cloud/apigateway-service/src/main/java/com/example/apigatewayservice/filter/GlobalFilter.java
@@ -1,5 +1,6 @@
 package com.example.apigatewayservice.filter;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -10,8 +11,8 @@ import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
-    public CustomFilter() {
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+    public GlobalFilter() {
         super(Config.class);
     }
 
@@ -21,14 +22,22 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
-            log.info("Custom PRE filter: request uri -> {}", request.getId());
-
+            log.info("Global Filter baseMessage: {}", config.getBaseMessage());
+            if(config.isPreLogger()) {
+                log.info("Global Filter Start: request id -> {}", request.getId());
+            }
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                log.info("Custom POST filter: response code -> {}", response.getStatusCode());
+                if(config.isPostLogger()) {
+                    log.info("Global Filter End: response code -> {}", response.getStatusCode());
+                }
             }));
         });
     }
 
+    @Data
     public static class Config{
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
     }
 }
