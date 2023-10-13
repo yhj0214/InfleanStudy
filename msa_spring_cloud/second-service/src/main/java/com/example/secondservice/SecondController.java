41047e4d1d6ca96da1bde7dbd4f0668d514cc540
유호재 <ho_0214@naver.com>
2023-10-13 23:30:04 +0900

    load blancer

--- msa_spring_cloud/second-service/src/main/java/com/example/secondservice/SecondController.java
+++ msa_spring_cloud/second-service/src/main/java/com/example/secondservice/SecondController.java
@@ -1,6 +1,10 @@
 package com.example.secondservice;
 
+import com.netflix.discovery.converters.Auto;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/second-service")
 public class SecondController {
+    Environment env;
+
+    @Autowired
+    public SecondController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome(){
         return "welcome second service";
@@ -22,7 +33,10 @@ public class SecondController {
 
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there. This is a message from Second Service.";
+    public String check(HttpServletRequest request){
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("Hi, there. This is a message from Second Service on PORT %s",
+                env.getProperty("local.server.port"));
     }
 }
