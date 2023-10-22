f0c7373aa34a7c811be01403482208a97cfcb699
유호재 <ho_0214@naver.com>
2023-10-23 02:12:15 +0900

    인증 전까지 완료 userservice spring security연동 해야함

---
+++ msa_spring_cloud/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,58 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
+import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+@Slf4j
+public class UserController {
+    private Environment env;
+    private UserService userService;
+
+    @Autowired
+    private Greeting greeting;
+
+
+    @Autowired
+    public UserController(Environment env, UserService userService) {
+        this.env = env;
+        this.userService = userService;
+    }
+
+    @GetMapping("/health-check")
+    public String status(){
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
+    }
+
+}
